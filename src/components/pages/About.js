54f5c38fa115edfe23be10dab984e922ac318648
Fernanda D'Emery <nandademery2@hotmail.com>
2019-12-03 15:38:45 +0100

    Fixed bugs

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,10 +3,10 @@ import React from "react";
 function About() {
   return (
     <React.Fragment>
-      <h1>About</h1>
+      <h1> About </h1>{" "}
       <p>
-        This is the TodoList app v1.0.0. It is part of a React Crash Course.
-      </p>
+        This is the TodoList app v1 .0 .0.It is part of a React Crash Course.{" "}
+      </p>{" "}
     </React.Fragment>
   );
 }
